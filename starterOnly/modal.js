eada8e8a8caf9f3e6bdcfb47b87042b72616e35b
Pedro Vinagre <pedro.vinagre@expleogroup.com>
2021-05-21 15:06:17 +0200

    correction HTML/CSS

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,7 +10,8 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+//const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelectorAll('.close');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +20,7 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
-
-
+// Lors du click sur le bouton "fermer" de la fenêtre de confirmation d'inscription, ferme et réinitialise de formulaire d'inscription.
+redCloseBtn.addEventListener('click', function() {
+	closeModal();
+})
