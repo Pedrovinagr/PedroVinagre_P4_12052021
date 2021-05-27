c27ee0a3bfc6e40786683e2f515514b6dfec0c17
Pedro Vinagre <pedro.vinagre@expleogroup.com>
2021-05-27 15:14:25 +0200

    correction HTML

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,19 +10,17 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelectorAll('.close');
-
-//const form = document.getElementById('reserve');
-//const first = document.getElementById('first');
-//const last = document.getElementById('last');
-//const email = document.getElementById('email');
-//const birthDate = document.getElementById('birthdate');
-//const quantity = document.getElementById('quantity');
-//const checkbox1 = document.getElementById('checkbox1');
-//const formData = document.getElementsByClassName('formData');
-//const btnSubmit = document.getElementById('btnSubmit');
-//const reserve = document.getElementById('reserve');
+const form = document.getElementById('reserve');
+const first = document.getElementById('first');
+const last = document.getElementById('last');
+const email = document.getElementById('email');
+const birthDate = document.getElementById('birthdate');
+const quantity = document.getElementById('quantity');
+const checkbox1 = document.getElementById('checkbox1');
+const formData = document.getElementsByClassName('formData');
+const btnSubmit = document.getElementById('btn_Submit');
+const reserve = document.getElementById('reserve');
 
 //------------------------------------#1 TODO : fermer le modale------------------------------
 // launch modal event
@@ -43,3 +41,17 @@ function closeModal() {
 }
 
 //------------------------------------#2 Validation des données ------------------------------
+
+//(1) Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
+
+function validateText(text) {
+	return (text.value !== '' && text.value.length >= 2);
+}
+
+last.addEventListener('change', function () {
+	refreshErrorMessage(
+		last.parentElement,
+		validateText(last),
+		'Veuillez entrer 2 caractères ou plus pour le champ du nom.'
+	)
+})
