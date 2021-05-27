1dfe013747855a3954e8f671dbc1567b0f95f8fc
Pedro Vinagre <pedro.vinagre@expleogroup.com>
2021-05-27 11:21:35 +0200

    correction CSS submit

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,16 +13,16 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelectorAll('.close');
 
-const form = document.getElementById('reserve');
-const first = document.getElementById('first');
-const last = document.getElementById('last');
-const email = document.getElementById('email');
-const birthDate = document.getElementById('birthdate');
-const quantity = document.getElementById('quantity');
-const checkbox1 = document.getElementById('checkbox1');
-const formData = document.getElementsByClassName('formData');
-const btnSubmit = document.getElementById('btnSubmit');
-const reserve = document.getElementById('reserve');
+//const form = document.getElementById('reserve');
+//const first = document.getElementById('first');
+//const last = document.getElementById('last');
+//const email = document.getElementById('email');
+//const birthDate = document.getElementById('birthdate');
+//const quantity = document.getElementById('quantity');
+//const checkbox1 = document.getElementById('checkbox1');
+//const formData = document.getElementsByClassName('formData');
+//const btnSubmit = document.getElementById('btnSubmit');
+//const reserve = document.getElementById('reserve');
 
 //------------------------------------#1 TODO : fermer le modale------------------------------
 // launch modal event
@@ -43,13 +43,3 @@ function closeModal() {
 }
 
 //------------------------------------#2 Validation des données ------------------------------
-
-// (1) Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
-function validateText(text) {
-	return (text.value !== '' && text.value.length >= 2);
-}
-
-// (3) L'adresse électronique est valide.
-function validateEmail(email) {
-	return (/^([A-Z|a-z|0-9](\.|_){0,1})+[A-Z|a-z|0-9]\@([A-Z|a-z|0-9])+((\.){0,1}[A-Z|a-z|0-9]){2}\.[a-z]{2,3}$/.test(email.value));
-}
